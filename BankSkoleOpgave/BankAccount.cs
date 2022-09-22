22f1748ce3b6e68968f20df29794ace8742cac30
Mathias Gedsted Christensen <math94s2@elev.techcollege.dk>
2022-09-22 11:03:36 +0200

    Add project files.

---
+++ BankSkoleOpgave/BankAccount.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSkoleOpgave
+{
+    public class BankAccount
+    {
+        private decimal Balance;
+
+        public BankAccount(decimal initialBankBalance)
+        {
+            Balance = initialBankBalance;
+        }
+
+        public void Withdraw()
+        {
+            Console.Write("Enter amount you wish to withdraw: ");
+            int amount = 0;
+            while (!int.TryParse(Console.ReadLine(), out amount))
+            {
+                Console.WriteLine("Please enter a number");
+                Console.Write("Enter amount you wish to withdraw: ");
+            }
+            if(amount < 0)
+            {
+                Console.WriteLine("Withdraw failed. please enter positive number");
+                return;
+            }
+
+            if ((Balance - amount) >= 0)
+            {
+                Balance -= amount;
+                Console.WriteLine("Withdrawal successful. New balance = " + Balance);
+            }
+            else
+            {
+                Console.WriteLine("Withdrawal failed. Balance cant go negative");
+            }
+
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine($"This is your balance: {Balance}");
+        }
+
+
+        public void Deposit()
+        {
+            Console.Write("Enter amount you wish to deposit: ");
+            int amount = 0;
+            while (!int.TryParse(Console.ReadLine(), out amount))
+            {
+                Console.WriteLine("Please enter a number");
+                Console.Write("Enter amount you wish to deposit: ");
+            }
+            if (amount < 0)
+            {
+                Console.WriteLine("Deposit failed. please enter positive number");
+                return;
+            }
+            Balance += amount;
+            Console.WriteLine("Deposit successful. New balance = " + Balance);
+        }
+    }
+
+
+
+
+
+
+}
---
+++ BankSkoleOpgave/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSkoleOpgave
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BankAccount bank1 = new BankAccount(100);
+            while (true)
+            {
+                Console.WriteLine("Pick your option");
+                Console.WriteLine("To show balance press 1");
+                Console.WriteLine("For deposit press 2");
+                Console.WriteLine("For withdrawal press 3");
+                Console.WriteLine();
+
+                ConsoleKeyInfo keyinput = Console.ReadKey(true);
+                int input = int.Parse(keyinput.KeyChar.ToString());
+
+                switch (input)
+                {
+                    case 1:
+                        bank1.GetBalance();
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        bank1.Deposit();
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        bank1.Withdraw();
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice!");
+                        Console.WriteLine();
+                        break;
+
+                }
+            }
+        }
+
+    }
+}
